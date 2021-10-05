cc0ef70a70a495693863432e368801886ce5636e
brannonstarnes <brannlee808@gmail.com>
2021-10-04 18:52:16 -0700

    resolved conflict relax.js

--- relax.js
+++ relax.js
@@ -171,7 +171,7 @@ Timer.prototype.timerCount = function () {
 
 let parsedUserMin = parseInt(parsedSettings.sessionTime);
 let sessionTimer = new Timer(parsedUserMin);
-sessionTimer.startTimer();
+
 
 getTimeofDay();
 getForeground();
@@ -201,6 +201,37 @@ function handleClick() {
 }
 
 
+
+function renderWelcome(){
+  let messageEl = document.getElementById('welcome');
+  messageEl.innerHTML=`<p>Welcome, ${parsedSettings.name}. Press 'Enter' to begin your journey.</p>`;
+}
+
+//fetch random quote from Go Quotes API
+
+// async function getApi2(url) {
+ 
+//   parentEl = document.getElementById('inspQuote');
+//   parentEl.style.animation = '';
+
+//   //get quote from API
+//   let response = await fetch(url);
+//   let data = await response.json();
+//   let quote = data.quotes[0]
+  
+//   //place quote using DOM
+//   parentEl.innerText = quote.text;
+//   parentEl.cite = quote.author;
+//   parentEl.style.animation = 'inspFly2 20s linear 1';
+
+//   setTimeout(getApi2, 20000, url);
+// }
+
+
+//Attribution
+//Inspirational quotes provided by <a href="https://goquotes.docs.apiary.io/" target="_blank">Go Quotes API</a>
+
+
 //-- SOUND SELECT --//
 
 function Sound(name, url) {
@@ -220,7 +251,7 @@ new Sound('whiteNoise', 'assets/sounds/industrial-hum.wav');
 function loadSound() {
   let player = document.getElementById('sound');
   let userSound = parsedSettings.sound;
-
+  console.log(player, userSound);
   for (let i = 0; i < Sound.library.length; i++) {
     let sound = Sound.library[i];
 
@@ -231,3 +262,32 @@ function loadSound() {
   }
 }
 loadSound();
+
+
+// event = keyup
+document.addEventListener('keyup', handleKeyup);
+  function handleKeyup(event){
+  if (event.code === 'Enter') {
+
+    //fade message
+    let messageEl = document.getElementById('welcome');
+    messageEl.style.opacity=0;
+    
+    //call api
+    getApi2(api_url)
+
+    //launch timer
+    sessionTimer.startTimer();
+
+    //launch music
+    document.getElementById('sound').paused=false;
+    document.removeEventListener('keyup', handleKeyup);
+  }
+};
+
+
+
+
+getTimeofDay();
+getForeground();
+renderWelcome();

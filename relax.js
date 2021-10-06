b689889c2120c30f7b30033e616e8a96b4cafac6
Edris Berg <ali_berg1@yahoo.com>
2021-10-06 10:07:08 -0700

    updated relax.css, relax.js, relax.html

--- relax.js
+++ relax.js
@@ -1,23 +1,39 @@
-let darkButton = document.querySelector('#dark-mode');
+let darkButton = document.getElementById('dark-mode');
+let darkImage = document.querySelector('darkImage');
+let lightImage = document.querySelector('lightImage');
+let bkgImage = document.getElementById('background');
+
+let logo = document.getElementById('logo');
 
 darkButton.addEventListener('click', (event) => {
-    document.body.style.backgroundColor = "red";
+
+    if (bkgImage.className === 'lightImage') {
+        bkgImage.className = 'darkImage'
+        //logo.className = 'darkImage';
+        darkButton.innerText = "Light Mode";
+        //darkButton.className = 'lightMode';
+    } else if (bkgImage.className === 'darkImage') {
+        bkgImage.className = 'lightImage'
+        darkButton.innerText = "Dark Mode";
+        //darkButton.className = 'lightMode';
+
+    }
+
 });
 
 
+// function darkMode() {
+//     var element = document.body;
+//     //var darkButton = document.getElementById("dark-mode");
 
-function darkMode() {
-    var element = document.body;
-    var content = document.getElementById("dark-mode");
+//     darkButton.innerText = "Light Mode";
+// }
 
-    content.innerText = "Light Mode";
-}
+// function lightMode() {
+//     var element = document.body;
+//     //var content = document.getElementById("light-mode");
 
-function lightMode() {
-    var element = document.body;
-    var content = document.getElementById("light-mode");
+//     darkButton.innerText = "Dark Mode";
 
-    content.innerText = "Dark Mode";
-
-}
+// }
 
